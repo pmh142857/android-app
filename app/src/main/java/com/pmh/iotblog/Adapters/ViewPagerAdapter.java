53a9ce45f8aacc69139daa0a5da297225b379a0a
pmh142857 <thehunter153180@gmail.com>
2020-07-19 15:32:59 +0700

    Finish OnBoard Screen

---
+++ app/src/main/java/com/pmh/iotblog/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,77 @@
+package com.pmh.iotblog.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.pmh.iotblog.R;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private  Context context;
+    private  LayoutInflater inflater;
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    private int images[] ={
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p3,
+    };
+
+    private String titles[] ={
+      "Buoc1",
+      "Buoc2",
+      "Buoc3"
+    };
+
+    private String descs[] ={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+    };
+
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout)object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View v   = inflater.inflate(R.layout.view_pager,container,false);
+
+        // init views
+        ImageView imageView = v.findViewById(R.id.imgViewPager);
+        TextView textTitle  = v.findViewById(R.id.txtTitleViewPager);
+        TextView textDesc   = v.findViewById(R.id.txtDescViewPager);
+
+        imageView.setImageResource(images[position]);
+        textTitle.setText(titles[position]);
+        textDesc.setText(descs[position]);
+
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
---
+++ app/src/main/java/com/pmh/iotblog/MainActivity.java
@@ -0,0 +1,49 @@
+package com.pmh.iotblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // pause app in 1.5s
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isFirstTime();
+            }
+        }, 1500);
+    }
+
+    private void isFirstTime() {
+        //
+        SharedPreferences preferences = getApplication().getSharedPreferences("onBoard", Context.MODE_PRIVATE) ;
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+        // default value true
+         if(isFirstTime){
+             // if true then  its firt time
+             SharedPreferences.Editor editor = preferences.edit();
+             editor.putBoolean("isFirstTime",false);
+             editor.apply();
+             // start OnBoard Activity
+             startActivity(new Intent(MainActivity.this, OnBoardActivity.class));
+             finish();
+         }
+         else{
+             // start Auth Activity
+             startActivity(new Intent(MainActivity.this, AuthActivity.class));
+             finish();
+
+         }
+    }
+}
